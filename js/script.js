bee8a0d4ee0c4d4fdae3d9cef0ceed24c52b22e4
Matthew <matsgeco92@hotmail.it>
2021-04-08 10:30:50 +0200

    Correzione esercizio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,48 @@
+// Metodo più efficiente
+
+function initVueEff() {
+  new Vue({
+    el:".vue",
+    data: {
+      images: ['img/immagine-1.jpg',
+                'img/immagine-2.jpg',
+                'img/immagine-3.jpg',
+                'img/immagine-4.jpg',
+                'img/immagine-5.jpg',
+    ],
+    imageInd: "0",
+  },
+
+
+
+    methods: {
+      nextClick: function () {
+        this.imageInd++
+        if (this.imageInd >= this.images.length) {
+
+          this.imageInd = 0;
+        }
+      },
+      prevClick: function () {
+        this.imageInd--;
+        if (this.imageInd < 0) {
+
+          this.imageInd = this.images.length - 1;
+        }
+      },
+    },
+
+    mounted: function(){
+      setInterval(this.nextClick,3000)
+    }
+  });
+}
+
 function initVue() {
   new Vue({
     el:".vue",
     data: {
-      image: 'img/immagine-',
+      images: 'img/immagine-',
       imageNum: "1",
       imageFormat: ".jpg"
     },
@@ -36,7 +76,8 @@ function initVue() {
 
 function init() {
 
-  initVue();
+  // initVue();
+  initVueEff();
 }
 
 $(init);
